8abcfeb2e1dd5f2725e5443475bcd2fa90e40729
ferrysetefanus <psandrezzz19@gmail.com>
2020-04-26 22:20:34 +0700

    memodifikasi form login

--- application/controllers/admin/Auth.php
+++ application/controllers/admin/Auth.php
@@ -3,7 +3,10 @@
 class Auth extends CI_Controller {
 
 	public function index() {
+
+		$this->load->view('template_admin/header');
 		$this->load->view('admin/login');
+		$this->load->view('template_admin/footer');
 	}
 }
 
